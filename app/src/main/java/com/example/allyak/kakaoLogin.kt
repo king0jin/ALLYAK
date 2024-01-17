a11a6a633b4ea091a3fd020335575140cef3059b
YOUNGJIN\et070 <et0709@naver.com>
2024-01-18 01:43:45 +0900

    '카카오톡 로그인'이미지도 drawable에 추가하였음 앱 실행시 보이는 첫 화면만 구현해 보았음 그리고 첫 화면에서 올약 이미지가 자동으로 위로 올라가는 애니메이션과 카카오톡 로그인 버튼이 서서히 나타나는 애니메이션을 추가하여 자연스럽게 로그인 화면으로 바뀌게 하고 싶음. => 김영진 문뜩 떠오른 생각 새로운 액티비티를 만들어서 만든 이유는 새로운 액티비티에서 로그인 관련 로직을 구현하고 필요한 뷰와 기능을 추가할 수 있고, 이렇게 구현된 액티비티는 앱의 다른 부분과 독립적으로 동작하니깐.. 필요한 경우에만 호출하여 사용할 수 있으니깐.. 액티비티 간의 전환과 데이터 전달을 액티비티 간 인텐트(Intent)를 사용하여 처리할 수 있잖아..? 예를 들어, 로그인이 성공하면 메인 화면으로 이동하도록 인텐트를 사용할 수 있으니깐... 암튼 여기까지 생각해 보았당

---
+++ app/src/main/java/com/example/allyak/kakaoLogin.kt
@@ -0,0 +1,11 @@
+package com.example.allyak
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class kakaoLogin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kakao_login)
+    }
+}

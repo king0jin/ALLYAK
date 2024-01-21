434db6239014533a62882e59260cfb9eb1b98ab7
YOUNGJIN\et070 <et0709@naver.com>
2024-01-21 13:00:11 +0900

    main액티비티에서 카카오로그인화면으로 자동 전환하게 하는 코드 추가.. 남은건.. 로그인 기능..^^ 아자아자 화이팅!

--- app/src/main/java/com/example/allyak/MainActivity.kt
+++ app/src/main/java/com/example/allyak/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.allyak
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // main에서 로그인화면으로 자동 전환 코드
+        val intent = Intent(this, kakaoLogin::class.java)
+        startActivity(intent)
+        finish()
+
     }
 }

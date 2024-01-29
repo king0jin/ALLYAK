63f75c6eb18317950a703b7d609a44fca14a4852
YOUNGJIN\et070 <et0709@naver.com>
2024-01-29 20:09:41 +0900

    주석처리한 코드 삭제

--- app/src/main/java/com/example/allyak/LoginApplication.kt
+++ app/src/main/java/com/example/allyak/LoginApplication.kt
@@ -5,15 +5,9 @@ import com.kakao.sdk.common.KakaoSdk
 
 class LoginApplication : Application(){
 
-    //companion object{
-//        var appContext : Context? = null
-    //}
-    // Kakao Sdk 초기화
+//  Kakao Sdk 초기화
     override fun onCreate() {
         super.onCreate()
-        //appContext = this
         KakaoSdk.init(this, getString(R.string.kakao_app_key))
-
     }
-
 }

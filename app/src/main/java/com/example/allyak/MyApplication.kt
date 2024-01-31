34196a65027c2119ecd6f1e9cbcfea8c7f375e85
YOUNGJIN\et070 <et0709@naver.com>
2024-01-31 18:18:28 +0900

    로그인 기능 수정

--- app/src/main/java/com/example/allyak/MyApplication.kt
+++ app/src/main/java/com/example/allyak/MyApplication.kt
@@ -4,14 +4,14 @@ import android.app.Application
 import com.kakao.sdk.common.KakaoSdk
 import com.naver.maps.map.NaverMapSdk
 
-class LoginApplication : Application(){
+class MyApplication : Application(){
 
 
     override fun onCreate() {
         super.onCreate()
         //  Kakao Sdk 초기화
         KakaoSdk.init(this, getString(R.string.kakao_app_key))
-        // API를 호출해 클라이언트 ID를 지정
+        // MAPAPI를 호출해 클라이언트 ID를 지정
         NaverMapSdk.getInstance(this).client =
             NaverMapSdk.NaverCloudPlatformClient("u3lhdtxvx3")
     }
--- app/src/main/java/com/example/allyak/kakaoLogin.kt
+++ app/src/main/java/com/example/allyak/kakaoLogin.kt
@@ -1,15 +1,14 @@
 package com.example.allyak
 
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatImageButton
-import com.kakao.sdk.auth.model.OAuthToken
-import com.kakao.sdk.user.UserApiClient
 
 
 class kakaoLogin : AppCompatActivity(){
+
+    private val kakaoAuthViewModle : KakaoAuthViewModle by viewModels()
 
     private var kakaoLoginButton: AppCompatImageButton? = null
 
@@ -17,41 +16,14 @@ class kakaoLogin : AppCompatActivity(){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_kakao_login)
 
+        //카카오 로그인 버튼 클릭시, handleKakaoLogin() 함수 실행
         kakaoLoginButton = findViewById(R.id.kakaobtn)
-
-        //카카오계정으로 로그인 공통 callback 구성
-        //카카오톡으로 로그인 할 수 없어 카카오톡계정으로 로그인할 경우 사용
-        val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
-            if (error != null) {
-                //로그인 실패
-                Log.e("kakaoLogin", "카카오계정으로 로그인 실패", error)
-            }
-            else if (token != null) {
-                //로그인 성공
-                Log.i("kakaoLogin", "카카오 계정으로 로그인 성공 ${token.accessToken}")
-                //HomeFragment로 이동하는 코드 - GoHome()으로 구현할 예정
-                GoHome()
-            }
+        kakaoLoginButton?.setOnClickListener {
+            kakaoAuthViewModle.kakaoLogin()
         }
-        //카카오 로그인
-        kakaoLoginButton?.setOnClickListener() {
-            //카카오 로그인 버튼이 클릭되었을 때 동작 구현
-            if(UserApiClient.instance.isKakaoTalkLoginAvailable((this@kakaoLogin))) {
-                UserApiClient.instance.loginWithKakaoTalk(this@kakaoLogin, callback = callback)
-            } else {
-                UserApiClient.instance.loginWithKakaoAccount(this@kakaoLogin, callback = callback)
 
-            }
-
-        }
 
     }
 
-    fun GoHome(){
-        //HomeFragment로 이동하는 코드
-        val intent = Intent(this, HomeFragment::class.java)
-        startActivity(intent)
-        finish()
-    }
 
 }

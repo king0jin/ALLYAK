7703ee78d05f281a0893357794092847f5033ddf
YOUNGJIN\et070 <et0709@naver.com>
2024-02-03 14:52:17 +0900

    지도 보여유, 서치뷰도 보여유

--- app/src/main/java/com/example/allyak/MapFragment.kt
+++ app/src/main/java/com/example/allyak/MapFragment.kt
@@ -6,14 +6,20 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.CameraAnimation
 import com.naver.maps.map.CameraUpdate
 import com.naver.maps.map.MapView
 import com.naver.maps.map.NaverMap
+import com.naver.maps.map.NaverMapSdk
 import com.naver.maps.map.overlay.Marker
 
+//private val LOCATION_PERMISSION_REQUEST_CODE : Int = 1000
 class MapFragment : Fragment(){
     lateinit var naverMap: NaverMap
     lateinit var mapView: MapView
+//    lateinit var locationSource: FusedLocationSource
+//    lateinit var requestPermissionLauncher: ActivityResultLauncher<String>
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,19 +29,36 @@ class MapFragment : Fragment(){
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+//        locationSource = FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
+        NaverMapSdk.getInstance(requireContext()).client = NaverMapSdk.NaverCloudPlatformClient("u3lhdtxvx3")
         //return inflater.inflate(R.layout.fragment_map, container, false)
         val rootMapView = inflater.inflate(R.layout.fragment_map, container, false)
         mapView = rootMapView.findViewById(R.id.map_view)
         mapView.onCreate(savedInstanceState)
+//        mapView.getMapAsync(this)
+        //locationSource = FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
         mapView.getMapAsync { naverMap ->
             this.naverMap = naverMap
             setupMap()
-
-
         }
+//        requestPermissionLauncher = registerForActivityResult(
+//            ActivityResultContracts.RequestPermission()) { isGrantes ->
+//            if(isGrantes) {
+//                setupMap()
+//            } else {
+//                Toast.makeText(requireContext(), "위치 권한을 허용해주세요.", Toast.LENGTH_SHORT).show()
+//            }
+//        }
         return rootMapView
 
     }
+
+//    override fun onMapReady(naverMap: NaverMap) {
+//        this.naverMap = naverMap
+////        naverMap.locationSource = locationSource
+//        naverMap.locationTrackingMode = LocationTrackingMode.Follow
+//        setupMap()
+//    }
 
     override fun onStart() {
         super.onStart()
@@ -75,19 +98,85 @@ class MapFragment : Fragment(){
     private fun setupMap() {
         val initialPosition = LatLng(37.5666103, 126.9783882)
         val cameraUpdate = CameraUpdate.scrollTo(initialPosition)
-            .animate(com.naver.maps.map.CameraAnimation.Easing)
+            .animate(CameraAnimation.Easing)
 
         naverMap.moveCamera(cameraUpdate)
+
         val marker = Marker()
         marker.position = initialPosition
         marker.map = naverMap
+//        if (ActivityCompat.checkSelfPermission(
+//                requireContext(),
+//                Manifest.permission.ACCESS_FINE_LOCATION
+//            ) == PackageManager.PERMISSION_GRANTED
+//        ) {
+//            naverMap.locationTrackingMode = LocationTrackingMode.Follow
+//            naverMap.locationSource = locationSource
+//            naverMap.uiSettings.isLocationButtonEnabled = true
+//            naverMap.uiSettings.isCompassEnabled = true
+//            val lastLocation = locationSource.lastLocation
+//            lastLocation?.let {
+//                val initialPosition = LatLng(it.latitude, it.longitude)
+//                val cameraUpdate = CameraUpdate.scrollTo(initialPosition)
+//                    .animate(CameraAnimation.Easing)
+//                naverMap.moveCamera(cameraUpdate)
+//                val marker = Marker()
+//                marker.position = initialPosition
+//                marker.map = naverMap
+//            } ?: run {
+//                Toast.makeText(requireContext(), "현재 위치를 찾을 수 없습니다.", Toast.LENGTH_SHORT).show()
+//            }
+//        } else {
+////            requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+//            return
+//
+//        }
+
+//        naverMap.locationTrackingMode = LocationTrackingMode.Follow
+//        naverMap.locationSource = locationSource
+//        val lastLocation = locationSource.lastLocation
+//        lastLocation?.let {
+//            val initalPosition = LatLng(it.latitude, it.longitude)
+//            val cameraUpdate = CameraUpdate.scrollTo(initalPosition)
+//                .animate(com.naver.maps.map.CameraAnimation.Easing)
+//            naverMap.moveCamera(cameraUpdate)
+//            val marker = Marker()
+//            marker.position = initalPosition
+//            marker.map = naverMap
+//        } ?: run {
+//            Toast.makeText(requireContext(), "현재 위치를 찾을 수 없습니다.", Toast.LENGTH_SHORT).show()
+//
+//        }
+
+
 
     }
-
-//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-//        super.onViewCreated(view, savedInstanceState)
-//        mapView = view.findViewById(R.id.map)
+//    override fun onRequestPermissionsResult(
+//        requestCode: Int,
+//        permissions: Array<out String>,
+//        grantResults: IntArray
+//    ) {
+//        when (requestCode) {
+//            LOCATION_PERMISSION_REQUEST_CODE -> {
+//                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                    setupMap()
+//                } else {
+//                    Toast.makeText(requireContext(), "위치 권한을 허용해주세요.", Toast.LENGTH_SHORT)
+//                        .show()
 //
+//                }
+//
+//            }
+//        }
+//    }
+
+//    override fun onLocationChanged(location: Location?) {
+//        location?.let {
+//            val newPosition = LatLng(it.latitude, it.longitude)
+//            val cameraUpdate = CameraUpdate.scrollTo(LatLng(it.latitude, it.longitude))
+//                .animate(CameraAnimation.Easing)
+//            this.naverMap.moveCamera(cameraUpdate)
+//        }
 //    }
 
 

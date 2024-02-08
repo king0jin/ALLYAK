f4eaa54d546802ebb67f15a2f3c5a63cd7f19c25
soo5o <jungsk20@gmail.com>
2024-02-08 19:14:58 +0900

    community firebase realtime database로 변경

--- app/src/main/java/com/example/allyak/ItemData.kt
+++ app/src/main/java/com/example/allyak/ItemData.kt
@@ -1,10 +1,10 @@
 package com.example.allyak
 
-class ItemData {
-    var docId: String? = null
-    var title: String? = null
-    var content: String? = null
-    var date: String? = null
-    var like: String? = null
-    var comment: String? = null
-}
+data class ItemData (
+    var docId: String? = null,
+    var title: String? = null,
+    var content: String? = null,
+    var date: String? = null,
+    var like: Int? = null,
+    var comment: Int? = null
+)
--- app/src/main/java/com/example/allyak/MyApplication.kt
+++ app/src/main/java/com/example/allyak/MyApplication.kt
@@ -1,8 +1,11 @@
 package com.example.allyak
 
+import android.util.Log
 import androidx.multidex.MultiDexApplication
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.kakao.sdk.common.KakaoSdk
+import com.kakao.sdk.user.UserApiClient
 import com.naver.maps.map.NaverMapSdk
 
 class MyApplication :MultiDexApplication(){  //원래 :Application() 이었음
@@ -11,17 +14,18 @@ class MyApplication :MultiDexApplication(){  //원래 :Application() 이었음
 //    }
 //수경 코드
     companion object {
-        //lateinit var auth: FirebaseAuth 인증 관리
+        lateinit var auth: FirebaseAuth //인증 관리
         lateinit var db: FirebaseFirestore
-/*        fun checkAuth(): Boolean {
+
+        fun checkAuth(): Boolean {
             var currentUser = auth.currentUser
             return currentUser?.let {
-                email = currentUser.email
+                //email = currentUser.email
                 currentUser.isEmailVerified
             } ?: let {
                 false
             }
-        }*/
+        }
     }
 
     override fun onCreate() {
@@ -33,6 +37,17 @@ class MyApplication :MultiDexApplication(){  //원래 :Application() 이었음
         // MAPAPI를 호출해 클라이언트 ID를 지정
         NaverMapSdk.getInstance(this).client =
             NaverMapSdk.NaverCloudPlatformClient("u3lhdtxvx3")
+        UserApiClient.instance.me { user, error ->
+            if (error != null) {
+                Log.e("Allyak", "사용자 정보 요청 실패", error)
+            }
+            else if (user != null) {
+                Log.i("Allyak", "사용자 정보 요청 성공" +
+                        "\n회원번호: ${user.id}" +
+                        "\n이메일: ${user.kakaoAccount?.email}" +
+                        "\n닉네임: ${user.kakaoAccount?.profile?.nickname}")
+            }
+        }
     }
 //    override fun onTerminate() {
 //        super.onTerminate()
